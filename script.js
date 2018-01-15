b0caaa7807a0f67e265e107b6b1cc0d4f4f9f641
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-15 14:18:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,6 @@
   var images = [
     "reddot.jpeg",
     "girl-about-red-blue-eyes-90757.jpeg",    
-    "paradise.jpeg",
     "walking_alone-wallpaper-1680x1050.jpg",
     "pinkrose.jpg",
     "ng-60686.jpg",
